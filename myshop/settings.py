715832c294e1ad6f370de35ac46828f5f410fef4
Mohit Kumar Mandhre <mohitmandhre@gmail.com>
2020-09-26 19:35:10 +0530

    django_heroku

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,5 @@ MEDIA_ROOT	=	os.path.join(BASE_DIR,	'media/')
 CART_SESSION_ID	=	'cart'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
